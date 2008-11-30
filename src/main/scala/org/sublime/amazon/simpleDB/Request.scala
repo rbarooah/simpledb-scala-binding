32c0cb6aefa94d5650b802d6fd95e79bf83765bd
Robin Barooah <robin@sublime.org>
2008-11-30 14:28:31 -0800

    work on specific parameters for CreateDomains, DeleteDomains, ListDomains, and PutAttributes

--- src/main/scala/org/sublime/amazon/simpleDB/Request.scala
+++ src/main/scala/org/sublime/amazon/simpleDB/Request.scala
@@ -1,33 +1,69 @@
 package org.sublime.amazon.simpleDB {
+	import Request._
 	
 	trait RequiredParameters {
 		def action:String
 		def awsAccessKeyId:String
 		def domainName:String
-		def signature:String
-		def signatureVersion:String
 		def timeStamp:String
-		def version:String
+		def version:String = "2007-11-07"
+		
+		def parameters = Map(
+			"Action" -> action, 
+			"AWSAccessKeyId" -> awsAccessKeyId,
+			"DomainName" -> domainName,
+			"Timestamp" -> timeStamp,
+			"Version" -> version
+		) ++ specificParameters
+		
+		def specificParameters:Map[String, String]
 	}
 	
 	trait CreateDomainRequest extends RequiredParameters {
 		def action = "CreateDomain"
+		def specificParameters = Map.empty
 	}
 	
 	trait DeleteDomainRequest extends RequiredParameters {
 		def action = "DeleteDomain"
+		def specificParameters = Map.empty
 	}
 	
 	trait ListDomains extends RequiredParameters {
 		def action = "ListDomains"
 		def maxNumberOfDomains:Option[String]
 		def nextToken:Option[String]
+		def specificParameters = 
+			optional("MaxNumberOfDomains", maxNumberOfDomains) ++
+			optional("NextToken", nextToken)		
+			
+		def optional (name:String, value:Option[String]) :Map[String,String] =
+			value match {
+				case Some(v) => Map(name->v)
+				case None => Map.empty
+			}				
 	}
 	
 	trait PutAttributes extends RequiredParameters {
 		def action = "PutAttributes"
 		def itemName:String
 		def attributes:Map[String, (String, Boolean)]
+		def specificParameters = attributeMap ++ Map("ItemName"->itemName) 
+		
+		def attributeMap :Map[String,String] = {
+			import scala.collection.immutable.HashMap;
+			var coded:Map[String, String] = new HashMap[String, String]();
+			var pos = 0;
+			
+			for (name <- attributes.keys) {
+				coded = coded + ("Attribute."+pos+"."+name -> attributes(name)._1)
+				if (attributes(name)._2) {
+					coded = coded + ("Attribute."+pos+".Replace" -> "true")
+				}
+				pos = pos + 1;
+			}
+			coded
+		}
 	}
 	
 	trait DeleteAttributes extends RequiredParameters {
@@ -55,5 +91,5 @@ package org.sublime.amazon.simpleDB {
 		def maxNumberOfItems:int
 		def nextToken:String
 		def queryExpression:String
-	}
+	}	
 }
