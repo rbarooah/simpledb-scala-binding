819b525313346b7e2bba81cdcbf290f4546bb0ae
Robin Barooah <robin@sublime.org>
2008-11-30 16:41:02 -0800

    add the first parsed result

--- src/main/scala/org/sublime/amazon/simpleDB/Response.scala
+++ src/main/scala/org/sublime/amazon/simpleDB/Response.scala
@@ -1,5 +1,11 @@
 package org.sublime.amazon.simpleDB {
 	import scala.xml._
+	import XMLFields._
+	
+	class ResponseMetaData (implicit xml:NodeSeq) {
+		val requestId = string("RequestId")
+		val boxUsage = double("BoxUsage")
+	}
 	
 	/**
 	 * Trait for breaking down XML
@@ -11,6 +17,7 @@ package org.sublime.amazon.simpleDB {
 		def string (name:String) (implicit xml:NodeSeq) = node(name) text
 		def dateField (name:String) (implicit xml:NodeSeq) = dateFormat.parse(string(name))
 		def int (name:String) (implicit xml:NodeSeq) = Integer.parseInt(string(name))
+		def double (name:String) (implicit xml:NodeSeq) = java.lang.Double.parseDouble(string(name))
 		def boolean (name:String) (implicit xml:NodeSeq) = string(name) match { 
 				case "True" => true
 				case "False" => false
