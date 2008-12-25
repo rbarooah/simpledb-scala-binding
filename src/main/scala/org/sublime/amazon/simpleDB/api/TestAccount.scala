f177d204f23caebc66df4bc41013e3706de421e7
Robin Barooah <robin@sublime.org>
2008-12-25 15:17:55 -0800

    Add test account that loads AWS properties from a properties file on the classpath.

---
+++ src/main/scala/org/sublime/amazon/simpleDB/api/TestAccount.scala
@@ -0,0 +1,53 @@
+// Copyright 2008 Robin Barooah
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.sublime.amazon.simpleDB.api {
+    import scala.xml._
+    
+    /**
+     * Convenient way to read test account details from a properties file on the classpath useful
+     * for running tests or operating from the console.
+     */
+    object Test {
+        import java.util.Properties
+        
+        private val resource = "/simpleDBTest.properties"
+        private val idProperty = "awsAccessKeyId"
+        private val keyProperty = "awsSecretKey"
+        
+        def propertyMissing (p:String) = 
+            throw new RuntimeException(resource + " does not define "+p)
+        
+        def using [T] (p:String) (next: String => T) (implicit prop:Properties) :T = {
+            val v = prop.getProperty(p)
+            if (v == null) propertyMissing(p)
+            else next(v)
+        }         
+        
+        def loadAccount :Option[SimpleAPI] = {
+            val is = getClass.getResourceAsStream(resource)
+            if (is == null) throw new RuntimeException("resource "+resource+" not on classpath")
+            else {
+                Console.println("found file")
+                implicit val p = new Properties()
+                p.load(is)
+                using (idProperty) { id =>
+                    using (keyProperty) { key =>
+                        Some (new SimpleDBAccount(id, key))
+                    }
+                }
+            }
+        }
+    }    
+}
